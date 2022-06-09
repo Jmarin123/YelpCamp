ff133019b4e48c230a3da2e239c1fdd1b9d4ae1c
Jason Osorio <waterdude50@gmail,com>
2022-06-09 02:36:56 -0400

    new style bootstrap

--- Seeds/index.js
+++ Seeds/index.js
@@ -18,10 +18,14 @@ const grabElement = arr => arr[Math.floor(Math.random() * arr.length)];
 const seedDb = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 60; i++) {
-        const valueOfSeeds = Math.floor(Math.random() * 1000)
+        const valueOfSeeds = Math.floor(Math.random() * 1000);
+        const randomPrice = Math.floor(Math.random() * 30);
         const c = new Campground({
             location: `${cities[valueOfSeeds].city}, ${cities[valueOfSeeds].state}`,
-            title: `${grabElement(descriptors)} ${grabElement(places)}`
+            title: `${grabElement(descriptors)} ${grabElement(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic magnam exercitationem quaerat cumque doloribus ea ipsum nam asperiores aliquam quam, architecto, alias, eius quas minima nisi sint? Illo, rerum voluptas.',
+            price: randomPrice
         });
         await c.save();
     }
